bac62bf4225b4413408b3a5883cff03639e82458
Kamil Hismatullin <kamil_hism@mail.ru>
2013-11-01 20:14:04 +0400

    spec example names template

--- spec/stack_func_testing_spec.rb
+++ spec/stack_func_testing_spec.rb
@@ -1,5 +1,74 @@
 require_relative '../stack'
 
 describe Stack do
+  before(:each) do
+    @stack = Stack.new
+  end
 
+  describe '#push' do
+    context 'когда стек пуст' do
+      it 'добавляется новый элемент' do
+      end
+      it 'размер стека увеличивается на 1' do
+      end
+    end
+
+    context 'когда в стеке уже есть элементы' do
+      it 'добавляется новый элемент' do
+      end
+      it 'размер стека увеличивается на 1' do
+      end
+    end
+
+    context 'когда происходит вызов метода без параметра' do
+      it 'размер стека не изменяется' do
+      end
+    end
+  end
+
+  describe '#pop' do
+    context 'когда стек пуст' do
+      it 'возвращается ничего' do
+      end
+      it 'размер стека не изменяется' do
+      end
+    end
+
+    context 'когда в стеке больше 1-го элемента' do
+      it 'возвращается верхний элемент' do
+      end
+      it 'размер стека уменьшается на 1' do
+      end
+    end
+
+    context 'когда в стеке 1 элемент' do
+      it 'возвращается верхний элемент' do
+      end
+      it 'размер стека равняется 0' do
+      end
+    end
+  end
+
+  describe '#peek' do
+    context 'когда стек пуст' do
+      it 'возвращается ничего' do
+      end
+      it 'размер стека не изменяется' do
+      end
+    end
+
+    context 'когда в стеке больше 1-го элемента' do
+      it 'возвращается верхний элемент' do
+      end
+      it 'размер стека не изменяется' do
+      end
+    end
+
+    context 'когда в стеке 1 элемент' do
+      it 'возвращается верхний элемент' do
+      end
+      it 'размер стека не изменяется' do
+      end
+    end
+  end
 end
